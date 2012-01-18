9befeef6b4d59cef04957bcd6d232ec34efe4842
Alex Chesser <achesser@klick.com>
2012-01-18 13:19:25 -0500

    first round of setup, wp runs but code coverage does not (wp-config and database required)

---
+++ PHPUnit/tests/FooTest.php
@@ -0,0 +1,17 @@
+<?php
+class StackTest extends PHPUnit_Framework_TestCase
+{
+    public function testPushAndPop()
+    {
+		// this is a single test to see if tests are running.
+        $stack = array();
+        $this->assertEquals(0, count($stack));
+ 
+        array_push($stack, 'foo');
+        $this->assertEquals('foo', $stack[count($stack)-1]);
+        $this->assertEquals(1, count($stack));
+ 
+        $this->assertEquals('foo', array_pop($stack));
+        $this->assertEquals(0, count($stack));
+    }
+}
